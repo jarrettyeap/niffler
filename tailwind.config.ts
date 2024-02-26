b46272336887c6d445ae4e48a894b0dd9296382e
Jarrett Yeap <3738154+jarrettyeap@users.noreply.github.com>
2024-02-26 12:51:08 +0800

    style: add new `error` color into theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -36,6 +36,10 @@ const config = {
           DEFAULT: "hsl(var(--destructive))",
           foreground: "hsl(var(--destructive-foreground))",
         },
+        error: {
+          DEFAULT: "hsl(var(--error))",
+          foreground: "hsl(var(--error-foreground))",
+        },
         muted: {
           DEFAULT: "hsl(var(--muted))",
           foreground: "hsl(var(--muted-foreground))",
